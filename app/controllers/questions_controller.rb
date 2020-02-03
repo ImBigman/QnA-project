07483ba090b403160b167cceeda3c8a05225fb6c
ImBigman <b1gmanever@gmail.com>
2020-02-03 16:23:11 +0300

    rewrite question controller and specs with expose gem

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,20 +1,10 @@
 class QuestionsController < ApplicationController
-
-  def index
-    @questions = Question.all
-  end
-
-  def show; end
-
-  def new; end
-
-  def edit; end
+  expose(:questions)
+  expose(:question)
 
   def create
-    @question = Question.new(question_params)
-
-    if @question.save
-      redirect_to @question
+    if question.save
+      redirect_to question
     else
       render :new
     end
@@ -35,11 +25,11 @@ class QuestionsController < ApplicationController
 
   private
 
-  def question
-    @question ||= params[:id] ? Question.find(params[:id]) : Question.new
-  end
-
-  helper_method :question
+  # def question
+  #   @question ||= params[:id] ? Question.find(params[:id]) : Question.new
+  # end
+  #
+  # helper_method :question
 
   def question_params
     params.require(:question).permit(:title, :body)
