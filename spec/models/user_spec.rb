823aa0224c18e6acc79045c5280c6e503b6a4ecb
ImBigman <b1gmanever@gmail.com>
2020-02-10 13:57:47 +0300

    add owner? method spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,16 @@ RSpec.describe User, type: :model do
 
   it { should validate_presence_of :email }
   it { should validate_presence_of :password }
+
+  let(:user) { create(:user) }
+  let(:user1) { create(:user) }
+  let(:question) { create(:question, user: user) }
+
+  it 'owns question' do
+    expect(user.owner?(question)).to be true
+  end
+
+  it 'Other user is not the owner of the question' do
+    expect(user1.owner?(question)).to be false
+  end
 end
