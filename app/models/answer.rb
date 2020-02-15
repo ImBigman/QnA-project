9519cadd25f0943f354025bf4da33c2692d7caa4
ImBigman <b1gmanever@gmail.com>
2020-02-15 12:56:10 +0300

    add AJAX methods, rewrite spec for js, add make_better method

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,4 +3,18 @@ class Answer < ApplicationRecord
   belongs_to :user
 
   validates :body, presence: true, length: { minimum: 10 }
+
+  scope :by_worth, -> { where(best: 'true') }
+
+  def up_to_best
+    update(best: 'true')
+  end
+
+  def usual
+    update(best: 'false')
+  end
+
+  def best?
+    best == 'true'
+  end
 end
--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -6,4 +6,37 @@ RSpec.describe Answer, type: :model do
 
   it { should validate_presence_of :body }
   it { should validate_length_of(:body).is_at_least(10) }
+
+  let(:user) { create(:user) }
+  let(:question) { create(:question, user: user) }
+  let(:answer) { create(:answer, user: user, question: question, best: 'true') }
+  let(:answer1) { create(:answer, user: user, question: question, best: 'false') }
+
+  it 'is the best?' do
+    expect(answer).to be_best
+  end
+
+  it 'is not the best?' do
+    expect(answer1).to_not be_best
+  end
+
+  describe 'by_worth scope' do
+    it 'includes answer with best: true' do
+      expect(Answer.by_worth).to include(answer)
+    end
+
+    it 'excludes answer without best: false' do
+      expect(Answer.by_worth).not_to include(answer1)
+    end
+  end
+
+  it 'up_to_best make answer the best' do
+    answer1.up_to_best
+    expect(answer1.best).to eq 'true'
+  end
+
+  it 'usual make answer usual' do
+    answer.usual
+    expect(answer.best).to eq 'false'
+  end
 end
