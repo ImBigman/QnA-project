cef25bd9540252bd25e6602452070ffb6e5d1bf2
ImBigman <b1gmanever@gmail.com>
2020-03-17 23:15:27 +0300

    delete commented lines

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,5 @@
 class ApplicationController < ActionController::Base
-  # before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :gon_user
-
-  # def configure_permitted_parameters
-  #   update_attrs = [:email]
-  #   devise_parameter_sanitizer.permit :account_update, keys: update_attrs
-  # end
 
   private
 
