cdf150ca100854198e9862617238d51fd03a31c7
ImBigman <b1gmanever@gmail.com>
2020-04-14 21:18:13 +0300

    add caching optimization

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,11 @@ module ApplicationHelper
   def javascript_link_tag(link)
     render inline: '<script src=' + link.url + '.js></script>'
   end
+
+  def collection_cache_keys_for(model)
+    klass = model.to_s.capitalize.constantize
+    count = klass.count
+    max_updated_at = klass.maximum(:updated_at).try(:utc).try(:to_s, :number)
+    "#{model.to_s.pluralize}/collection-#{count}-#{max_updated_at}"
+  end
 end
